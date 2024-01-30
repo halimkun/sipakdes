7088c012acd465a184b09d2696d644e30c322e21
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-01-30 22:56:57 +0700

    Add Auth routes and extend AuthController from BaseController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use Config\Auth as AuthConfig;
 
 /**
  * @var RouteCollection $routes
@@ -11,3 +12,46 @@ $routes->get('/', 'Home::index');
 // Admin
 $routes->get('/admin', "Dashboard::toIndex");
 $routes->get('/admin/dashboard', "Dashboard::index");
+
+
+// Operator Kelurahan
+
+
+
+// Operator Posyandu
+
+
+
+// Warga
+
+
+
+
+
+
+// --- ------------- Auth 
+// Myth:Auth routes file.
+$routes->group('', ['namespace' => 'App\Controllers'], static function ($routes) {
+    // Load the reserved routes from Auth.php
+    $config         = config(AuthConfig::class);
+    $reservedRoutes = $config->reservedRoutes;
+
+    // Login/out
+    $routes->get($reservedRoutes['login'], 'AuthController::login', ['as' => $reservedRoutes['login']]);
+    $routes->post($reservedRoutes['login'], 'AuthController::attemptLogin');
+    $routes->get($reservedRoutes['logout'], 'AuthController::logout');
+
+    // Registration
+    $routes->get($reservedRoutes['register'], 'AuthController::register', ['as' => $reservedRoutes['register']]);
+    $routes->post($reservedRoutes['register'], 'AuthController::attemptRegister');
+
+    // Activation
+    $routes->get($reservedRoutes['activate-account'], 'AuthController::activateAccount', ['as' => $reservedRoutes['activate-account']]);
+    $routes->get($reservedRoutes['resend-activate-account'], 'AuthController::resendActivateAccount', ['as' => $reservedRoutes['resend-activate-account']]);
+
+    // Forgot/Resets
+    $routes->get($reservedRoutes['forgot'], 'AuthController::forgotPassword', ['as' => $reservedRoutes['forgot']]);
+    $routes->post($reservedRoutes['forgot'], 'AuthController::attemptForgot');
+    $routes->get($reservedRoutes['reset-password'], 'AuthController::resetPassword', ['as' => $reservedRoutes['reset-password']]);
+    $routes->post($reservedRoutes['reset-password'], 'AuthController::attemptReset');
+});
