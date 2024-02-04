3cb573afa3bc9ee747222aa7af3c34b65de47145
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-02-04 11:59:16 +0700

    Update allowedFields in UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -17,7 +17,7 @@ class UserModel extends Model
     protected $returnType     = 'App\Entities\User';
     protected $useSoftDeletes = true;
     protected $allowedFields  = [
-        'email', 'username', 'kk', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash',
+        'email', 'username', 'id_penduduk', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash',
         'status', 'status_message', 'active', 'force_pass_reset', 'permissions', 'deleted_at',
     ];
     protected $useTimestamps   = true;
