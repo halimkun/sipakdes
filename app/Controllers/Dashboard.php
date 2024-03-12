9f9de52212d8fd357d50e01e9cd69d0c2d0972b5
Faisal Halim <ffaisalhalim@gmail.com>
2024-03-13 05:03:26 +0700

    Add penduduk metrics to dashboard view

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -7,6 +7,17 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Dashboard extends BaseController
 {
+    protected $pendudukModel;
+    protected $kematianModel;
+    protected $metrics;
+    protected $bgColors = ['bg-primary', 'bg-secondary', 'bg-success', 'bg-danger', 'bg-warning', 'bg-info'];
+
+    public function __construct()
+    {
+        $this->pendudukModel = new \App\Models\PendudukModel();
+        $this->kematianModel = new \App\Models\KematianModel();
+    }
+
     public function toIndex()
     {
         return redirect()->to('/dashboard', 301);
@@ -20,6 +31,48 @@ class Dashboard extends BaseController
                 ['title' => ucfirst(user()->username), 'url' => '/'],
                 ['title' => 'Dashboard', 'url' => '/dashboard', 'active' => true],
             ],
+
+            'metrics' => $this->getPendudukMetrics()
         ]);
+    }
+
+    protected function getSuratMetrics()
+    {
+        // ... query to get the metrics
+    }
+
+    protected function getPendudukMetrics()
+    {
+        $countPenduduk = $this->pendudukModel->countAll();
+        $countPendudukAnak = $this->pendudukModel->where('TIMESTAMPDIFF(YEAR, penduduk.tanggal_lahir, CURDATE()) <=', 17)->countAllResults();
+        $countPendudukDewasa = $this->pendudukModel->where('TIMESTAMPDIFF(YEAR, penduduk.tanggal_lahir, CURDATE()) >', 17)->countAllResults();
+        $countPendudukMeninggal = $this->kematianModel->countAll();
+        $countPendudukPerempuan = $this->pendudukModel->where('jenis_kelamin', 'Perempuan')->countAllResults();
+        $countPendudukLakiLaki = $this->pendudukModel->where('jenis_kelamin', 'Laki-laki')->countAllResults();
+
+        // random bg color
+        $this->metricsBuilder('penduduk', 'Jumlah Penduduk', $countPenduduk, ['icon' => 'fa fa-users', 'color' => 'bg-success']);
+        $this->metricsBuilder('penduduk', 'Penduduk Anak', $countPendudukAnak, ['icon' => 'fa fa-child', 'color' => 'bg-primary']);
+        $this->metricsBuilder('penduduk', 'Penduduk Dewasa', $countPendudukDewasa, ['icon' => 'fa fa-user', 'color' => 'bg-warning']);
+        $this->metricsBuilder('penduduk', 'Penduduk Meninggal', $countPendudukMeninggal, ['icon' => 'fa fa-user-times', 'color' => 'bg-danger']);
+        $this->metricsBuilder('penduduk', 'Penduduk Perempuan', $countPendudukPerempuan, ['icon' => 'fa fa-venus', 'color' => 'bg-secondary']);
+        $this->metricsBuilder('penduduk', 'Penduduk Laki-laki', $countPendudukLakiLaki, ['icon' => 'fa fa-mars', 'color' => 'bg-secondary']);
+        
+        return $this->metrics;
+    }
+
+    protected function metricsBuilder($key, $title, $jumlah, $options = [])
+    {
+        $this->metrics[$key][] = [
+            'title'   => $title,
+            'count'   => $jumlah,
+            'options' => $options,
+        ];
+    }
+
+    // funtion to get random bg color
+    protected function randomBgColor()
+    {
+        return $this->bgColors[array_rand($this->bgColors)];
     }
 }
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,4 +1,19 @@
-<?= $this->extend('layouts/app') ;?>
-<?= $this->section('content') ;?>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aperiam ducimus eaque esse, numquam impedit voluptate error incidunt reiciendis? Eum eveniet exercitationem totam illo commodi saepe atque nihil vitae aut.
-<?= $this->endSection() ;?>
+<?= $this->extend('layouts/app'); ?>
+<?= $this->section('content'); ?>
+<div class="mb-3">
+  <h5>Ringkasan Data Penduduk</h5>
+  <div class="row">
+    <?php foreach ($metrics['penduduk'] as $item) : ?>
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+          <span class="info-box-icon <?= $item['options']['color'] ?>"><i class="far <?= $item['options']['icon'] ?>"></i></span>
+          <div class="info-box-content">
+            <span class="info-box-text"><?= $item['title'] ?></span>
+            <span class="info-box-number"><?= $item['count'] ?></span>
+          </div>
+        </div>
+      </div>
+    <?php endforeach ?>
+  </div>
+</div>
+<?= $this->endSection(); ?>
